9d84adea7149cd228f4c19efcb4e8a74f74b1f90
Mayur Kumshi <mayur.kumshi@globant.com>
2019-07-19 15:16:36 +0530

    Committing to branch changes

---
+++ src/test/java/comproject/selenium/seleniumMVN011/TestNGProgramDemo1.java
@@ -0,0 +1,39 @@
+package comproject.selenium.seleniumMVN011;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNGProgramDemo1 
+{
+
+	WebDriver driver;
+	@Test
+	public void LoginCheck() throws Exception
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mayur.kumshi\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://google.com");
+		
+		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("maven dependency");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//input[@type='submit']//preceding::center")).click();
+				
+		Thread.sleep(5000);
+		
+		
+		String url = driver.getCurrentUrl();
+		
+		String url2= "https://mvnrepository.com/";
+		
+		System.out.println(" "+url);
+		
+		Assert.assertEquals(url, url2);
+		
+		driver.close();
+		
+	}
+	
+}

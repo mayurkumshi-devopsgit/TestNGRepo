bfbd6d7f67a78f3bb301c96a4de3a4f0d567e0b7
Mayur Kumshi <mayur.kumshi@globant.com>
2019-07-22 17:13:52 +0530

    Updated the code and added chromedriver.exe to project level

--- src/test/java/comproject/selenium/seleniumMVN011/TestNGProgramDemo1.java
+++ src/test/java/comproject/selenium/seleniumMVN011/TestNGProgramDemo1.java
@@ -11,30 +11,30 @@ public class TestNGProgramDemo1
 
 	WebDriver driver;
 	@Test
-	public void LoginCheck() throws Exception
-	{
-		System.out.println("Hi");
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mayur.kumshi\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
+	public void LoginCheck() throws Exception {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\mayur.kumshi\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
+		System.out.println("Opening the URL");
 		driver.get("http://google.com");
-		
+
 		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("maven dependency");
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//input[@type='submit']//preceding::center")).click();
-				
+
 		Thread.sleep(5000);
-		
-		
+
 		String url = driver.getCurrentUrl();
-		
-		String url2= "https://mvnrepository.com/";
-		
-		System.out.println(" "+url);
-		
+
+		String url2 = "https://mvnrepository.com/";
+
+		System.out.println(" " + url);
+
 		Assert.assertEquals(url, url2);
-		
+
 		driver.close();
-		
+
 	}
 	
 }
